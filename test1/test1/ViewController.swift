596996358ff5daed3c872293998ddfcd0e1c0f0f
杨子 <yangzi@yangzideMacBook-Pro.local>
2016-11-04 14:37:30 +0800

    第一次提交git本地仓库

---
+++ test1/test1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by 杨子 on 2016/11/4.
+//  Copyright © 2016年 Peter Young. All rights reserved.
+//
+
+import UIKit
+
+
+/// diyici
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
